b4d9be59c3a73cb9aba6ae75f6616f530e2bdd78
Nicolas VIENOT <nvienot@student.42.fr>
2019-02-07 20:21:26 +0100

    norm ft_init_img

--- ft_init_img.c
+++ ft_init_img.c
@@ -6,7 +6,7 @@
 /*   By: nvienot <nvienot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 19:02:49 by nvienot           #+#    #+#             */
-/*   Updated: 2019/02/06 17:56:26 by nvienot          ###   ########.fr       */
+/*   Updated: 2019/02/07 20:21:08 by nvienot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,12 @@ void		ft_refresh_img(t_win *win)
 {
 	mlx_destroy_image(win->mlx_ptr, win->img_ptr);
 	win->img_ptr = mlx_new_image(win->mlx_ptr, IMG_HOR_SIZE, IMG_VER_SIZE);
-	// win->data = mlx_get_data_addr(win->img_ptr, &win->bpp, &win->sizeline, &win->endian);
 }
 
 void		ft_refresh_background(t_win *win)
 {
-	// int	a;
-	// int b;
-
-	// a = 1700;
-	// b = 1100;
-	// mlx_destroy_image(win->mlx_ptr, win->img_background);
-	// win->img_background = mlx_new_image(win->mlx_ptr, 1700, 1100);
-	// win->img_background = mlx_xpm_file_to_image(win->mlx_ptr, "./fdf.xpm", &a, &b);
-	mlx_put_image_to_window(win->mlx_ptr, win->win_ptr, win->img_background, 0, 0);
+	mlx_put_image_to_window(win->mlx_ptr, win->win_ptr, \
+		win->img_background, 0, 0);
 }
 
 void		ft_init_img(t_win *win)
@@ -40,9 +32,11 @@ void		ft_init_img(t_win *win)
 	a = 1700;
 	b = 1100;
 	win->img_background = mlx_new_image(win->mlx_ptr, 1700, 1100);
-	win->img_background = mlx_xpm_file_to_image(win->mlx_ptr, "./fdf.xpm", &a, &b);
+	win->img_background = mlx_xpm_file_to_image(win->mlx_ptr, \
+		"./fdf.xpm", &a, &b);
 	win->img_ptr = mlx_new_image(win->mlx_ptr, IMG_HOR_SIZE, IMG_VER_SIZE);
-	win->data = mlx_get_data_addr(win->img_ptr, &win->bpp, &win->sizeline, &win->endian);
+	win->data = mlx_get_data_addr(win->img_ptr, &win->bpp, \
+		&win->sizeline, &win->endian);
 }
 
 void		mlx_put_pixel_to_image(t_win *win, int x, int y, int i)
@@ -52,7 +46,6 @@ void		mlx_put_pixel_to_image(t_win *win, int x, int y, int i)
 
 	octet = win->bpp / 8;
 	a = win->s[i]->color;
-
 	if (x >= 0 && y >= 0 && x < IMG_HOR_SIZE && y < IMG_VER_SIZE)
 	{
 		ft_memcpy(&win->data[octet * x + win->sizeline * y], &a, octet);
